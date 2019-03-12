78d0317636d002a092ff50839ea19e9ee31ba962
lialis <lialiseitbekova@gmail.com>
2019-03-12 15:20:37 +0700

    push all files

---
+++ app.js
@@ -0,0 +1,114 @@
+Ext.onReady(function(){ 
+	Ext.define('Name', {
+    extend: 'Ext.data.Model',
+    idProperty: 'id', 
+    fields: [{
+        name: 'firstName'
+    }, {
+        name: 'lastName'
+    }]
+	});
+
+	Ext.define('User', { 
+		extend: 'Ext.data.Model', 
+		idProperty: 'id', 
+		fields: [{ 
+			name: 'id', 
+			type: 'string' 
+		}, 
+		// { 
+		// 	name: 'firstName', 
+		// 	type: 'string', 
+		// }, 
+		// { 
+		// 	// name: 'name_lastName',
+		// 	name:'name', 
+			
+		// }, 
+		{ 
+			name: 'phone', 
+			type: 'string' 
+		}, 
+		{ 
+			name: 'about', 
+			type: 'string' 
+		}, 
+		{ 
+			name: 'eyeColor', 
+			type: 'string' 
+		}],
+		 hasMany: [{
+        model: 'Name',
+        name: 'name',
+        associationKey: 'name'
+    }] 
+	}); 
+
+
+	var store = Ext.create('Ext.data.Store', { 
+		model: 'User', 
+		autoLoad: true, 
+		pageSize: 10, 
+		// currentPage: 4, 
+		proxy: { 
+			type: 'ajax', 
+			url: 'info.json', 
+			reader: { 
+				type: 'json', 
+				root: 'users' 
+			} 
+		} 
+
+	}); 
+
+	Ext.create('Ext.grid.Panel', { 
+		title: 'Пользователи', 
+		height: 500, 
+		width: 550, 
+		margin:5, 
+		store: store, 
+		dockedItems: [{ 
+			xtype: 'pagingtoolbar', 
+			store:store, 
+			dock: 'bottom', 
+			displayInfo: true, 
+			beforePageText: 'Страница', 
+			afterPageText: 'из {0}', 
+			displayMsg: 'Пользователи {0} - {1} из {2}' 
+		}], 
+		columns: [{ 
+			xtype:'rownumberer' 
+		},
+		{ 
+			header: 'Имя', 
+			renderer: function(value, meta, record, colIndex, rowIndex, store, view) {
+        return record.name().first().get( 'firstName' );
+        // return record.getData().name.lastName;
+	    }
+		}, 
+		{ 
+			header: 'Фамилия',
+			id : 'id',    
+      flex:  1,   
+			// sortable: true,
+			hideable: true ,
+			renderer: function(value, meta, record, colIndex, rowIndex, store, view) {
+        return record.name().first().get( 'lastName' );
+	    }
+
+		}, 
+		{ 
+			header: 'Номер телефона', 
+			dataIndex: 'phone', 
+		}, 
+		{ 
+			header: 'Описание', 
+			dataIndex: 'about', 
+		}, 
+		{ 
+			header: 'Цвет глаз', 
+			dataIndex: 'eyeColor', 
+		}], 
+		renderTo: Ext.getBody() 
+	}); 
+});
---
+++ exa.js
@@ -0,0 +1,12 @@
+// Ext.define('MyApp.Application', {
+//     extend: 'Ext.app.Application',
+//     name: 'MyApp',
+ 
+// });
+
+// Ext.application('MyApp.Application');
+var ed = Ext.create('User', { name: 'Ed Spencer' });
+
+console.log(ed.data);
+const appp = 7;
+console.log(appp);
